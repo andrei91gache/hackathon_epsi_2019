c95723b04a7c6b40e20fe8b7707c2c1d1d2f8ae1
Andrei Gache <andrei.gache.99@gmail.com>
2019-04-27 14:34:47 +0200

    3update

--- bin/cron.php
+++ bin/cron.php
@@ -13,10 +13,10 @@ require __DIR__.'/../vendor/autoload.php';
 
 $file_parser = new \HackathonEpsi\src\bll\FileParser();
 //Download file from Meteo
-/*$file_parser->httpRequestGetJsonFromAPIMeteo();
+$file_parser->httpRequestGetJsonFromAPIMeteo();
 try{
     $file_parser->parseArrayAndSave();
 }catch (Exception $exception){
     print $exception->getMessage();
-}*/
+}
 $file_parser->httpRequestGetFileFromRTE();
