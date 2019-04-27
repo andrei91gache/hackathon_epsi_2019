045c89b0a9038f3931fc61132c78f166401fd4b9
Andrei Gache <andrei.gache.99@gmail.com>
2019-04-27 11:31:28 +0200

    first update

---
+++ src/bll/FileParser.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Filename: FileParser.php
+ * User: Andrei Gache
+ * Email: andrei.gache.99@gmail.com
+ * Website: https://www.andrei-gache.com/
+ * Date: 27/04/19
+ * Time: 11:26
+ */
+
+namespace HackathonEpsi\src\bll;
+
+
+class FileParser
+{
+    public function downloadFile()
+    {
+
+    }
+
+    public function parseFile()
+    {
+
+    }
+
+    public function saveIntoDatabase(){
+
+    }
+}
---
+++ src/dal/ConnectionDB.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Filename: ConnectionDB.php
+ * User: Andrei Gache
+ * Email: andrei.gache.99@gmail.com
+ * Website: https://www.andrei-gache.com/
+ * Date: 27/04/19
+ * Time: 11:24
+ */
+
+namespace HackathonEpsi\src\dal;
+
+
+class ConnectionDB
+{
+    public function __construct()
+    {
+
+    }
+}
