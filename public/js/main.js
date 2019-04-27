fcd6f36b7700dc2b1cd83b7d19c7a991c73d426f
Andrei Gache <andrei.gache.99@gmail.com>
2019-04-27 16:38:02 +0200

    update

--- public/js/main.js
+++ public/js/main.js
@@ -1,40 +1,52 @@
-var ctx = document.getElementById('myChart').getContext('2d');
-var myChart = new Chart(ctx, {
+var ctx1 = document.getElementById('production_actuelle').getContext('2d');
+var ctx2 = document.getElementById('consumation_actuelle').getContext('2d');
+
+new Chart(ctx1, {
     type: 'line',
+    // The data for our dataset
     data: {
-        labels: ["5:00", "5:30", "6:00", "6:30", "7:00", "7:30", "8:00", 
-        "8:30", "9:00", "9:30", "10:00", "10:30", "11:00", "11:30", "12:00", "12:30", "13:00", 
-        "13:30", "14:00", "14:30", "15:00", "15:30", "16:00", "16:30", "17:00", "17:30", "18:00", 
-        "18:30", "19:00", "19:30", "20:00", "20:30", "21:00" "21:30", "22:00", "22:30", "23:00"],
-        datasets: [{
-        fill: false,
-        lineTension: 0.1,
-        backgroundColor: "rgba(75, 192, 192, 0.1)",
-        borderColor: "rgba(75, 192, 192, 1)",
-        borderCapStyle: 'butt',
-        borderDash: [],
-        borderDashOffset: 0.0,
-        borderJoinStyle: 'mite',
-        pointBorderColor: "rgba(75, 192, 192, 1)",
-        pointBackgroundColor: "#fff",
-        pointBorderWidth: 1,
-        pointHoverRadius: 5,
-        pointHoverBackgroundColor: "rgba(75, 192, 192, 1)",
-        pointHoverBorderColor: "rgba(220, 220, 200, 1)",
-        pointHoverBorderWidth: 2,
-        pointRadius: 1,
-        pointHitRadius: 10,
-        data: [0, 0, 0, 0, 0, 3, 4, 5, 7, 9, 
-        12, 19, 26, 34, 56, 67, 78, 79, 78, 75, 
-        73, 78, 89, 56, 45, 34, 30, 28, 25, 21, 
-        16, 12, 9, 4, 3, 0, 0]
-        }]
+        labels: ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'] ,
+        datasets: [
+            {
+                label: "Production d'energie solaire",
+                borderColor: "#ff9900",
+                data: [15,0,8,155,22,35,78]
+            },
+        ]
     },
+    // Configuration options go here
     options: {
         scales: {
             yAxes: [{
                 ticks: {
-                    beginAtZero: true
+                    suggestedMin: 0,
+                    suggestedMax: 500
+                }
+            }]
+        }
+    }
+});
+
+new Chart(ctx2, {
+    type: 'line',
+    // The data for our dataset
+    data: {
+        labels: ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'] ,
+        datasets: [
+            {
+                label: "Consumation d'energie solaire",
+                borderColor: "#ff9900",
+                data: [25,88,30,0,2,12,3]
+            },
+        ]
+    },
+    // Configuration options go here
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    suggestedMin: 0,
+                    suggestedMax: 500
                 }
             }]
         }
